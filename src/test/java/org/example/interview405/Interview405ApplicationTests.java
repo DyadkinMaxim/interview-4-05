230c11c680005123f4d94854087fe1b3bb80ed78
Dyadkin Maxim <maxim.dyadkin@sharp-dev.net>
2026-05-04 15:35:27 +0200

    initial commit

---
+++ src/test/java/org/example/interview405/Interview405ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.interview405;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Interview405ApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
